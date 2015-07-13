4ba107861ce4241b40b9b940aa1ed16ac5c50d76
Lioman <dev@lioman.de>
2015-07-13 10:27:12 +0200

    Modularize Js, Map has not to be redrawn on year select.

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 function drawMap(year) {
     $('.map').empty();
 
-    var paxYear = 'pax' + year;
+    width = 650;
+    height = 600;
 
-    var width = 900,
-        height = 600;
-
-    var projection = d3.geo.mercator()
+    projection = d3.geo.mercator()
         .center([9, 55])
-        .scale(500)
+        .scale(460)
         .translate([width / 2, height / 2]);
 
-    var path = d3.geo.path()
+    path = d3.geo.path()
         .projection(projection)
         .pointRadius(2);
 
-    var svg = d3.select('.map').append('svg')
+    svg = d3.select('.map').append('svg')
         .attr('width', width)
         .attr('height', height);
 
-    var radius = d3.scale.sqrt()
+    radius = d3.scale.sqrt()
         .domain([0, 300000000])
         .range([0, 30]);
 
-    var formatNumber = d3.format('>-,');
-
+    formatNumber = d3.format('>-,');
     d3.json('build/eu.json', function (error, eu) {
         if (error) return console.error(error);
+        euJson = eu;
 
         svg.append('path')
             .datum(topojson.feature(eu, eu.objects.subunits))
@@ -37,7 +35,9 @@ function drawMap(year) {
             .data(topojson.feature(eu, eu.objects.subunits).features)
             .enter().append('path')
             .attr('class', function (d) {
-                return 'subunit ' + d.id;
+                if (d.id !=-99){
+                return 'subunit ' + d.id;}
+                return 'subunit NO';
             })
             .attr('d', path);
 
@@ -46,7 +46,10 @@ function drawMap(year) {
             .data(topojson.feature(eu, eu.objects.subunits).features)
             .enter().append('text')
             .attr('class', function (d) {
+                if (d.id !=-99){
                 return 'subunit-label ' + d.id;
+                }
+                return 'subunit-label NO';
             })
             .attr('transform', function (d) {
                 return 'translate(' + path.centroid(d) + ')';
@@ -56,53 +59,60 @@ function drawMap(year) {
                 return d.properties.name;
             });
 
-
-        svg.append('g')
-            .attr('class', 'bubble')
-            .selectAll('circle')
-            .data(topojson.feature(eu, eu.objects.subunits).features
-                .sort(function (a, b) {
-                    return b.properties[paxYear] - a.properties[paxYear];
-                }))
-            .enter().append('circle')
-            .attr('transform', function (d) {
-                return 'translate(' + path.centroid(d) + ')';
-            })
-            .attr('r', function (d) {
-                return radius(d.properties[paxYear]);
-            })
-            .append('title')
-            .text(function (d) {
-                return d.properties.name
-                    + '\nPax in ' + year + ':  ' + formatNumber(d.properties[paxYear]);
-            }).attr('class', 'bubbletext');
-
-
-        var legend = svg.append('g')
-            .attr('class', 'legend')
-            .attr('transform', 'translate(' + (width - 50) + ',' + (height - 20) + ')')
-            .selectAll('g')
-            .data([3000000, 30000000, 300000000])
-            .enter().append('g');
-
-        legend.append('circle')
-            .attr('cy', function (d) {
-                return -radius(d);
-            })
-            .attr('r', radius);
-
-        legend.append('text')
-            .attr('y', function (d) {
-                return -2 * radius(d);
-            })
-            .attr('dy', '1.3em')
-            .text(d3.format('.1s'));
-
+        drawBubbles(year);
     });
+
+
+}
+
+function drawBubbles(year) {
+    $('.bubble').empty();
+    var paxYear = 'pax' + year;
+
+    svg.append('g')
+        .attr('class', 'bubble')
+        .selectAll('circle')
+        .data(topojson.feature(euJson, euJson.objects.subunits).features
+            .sort(function (a, b) {
+                return b.properties[paxYear] - a.properties[paxYear];
+            }))
+        .enter().append('circle')
+        .attr('transform', function (d) {
+            return 'translate(' + path.centroid(d) + ')';
+        })
+        .attr('r', function (d) {
+            return radius(d.properties[paxYear]);
+        })
+        .append('title')
+        .text(function (d) {
+            return d.properties.name
+                + '\nPax in ' + year + ':  ' + formatNumber(d.properties[paxYear]);
+        }).attr('class', 'bubbletext');
+
+
+    var legend = svg.append('g')
+        .attr('class', 'legend')
+        .attr('transform', 'translate(' + (width - 50) + ',' + (height - 20) + ')')
+        .selectAll('g')
+        .data([3000000, 30000000, 300000000])
+        .enter().append('g');
+
+    legend.append('circle')
+        .attr('cy', function (d) {
+            return -radius(d);
+        })
+        .attr('r', radius);
+
+    legend.append('text')
+        .attr('y', function (d) {
+            return -2 * radius(d);
+        })
+        .attr('dy', '1.3em')
+        .text(d3.format('.1s'));
+
 }
 
 
-
-function setYear(sel){
-    drawMap(sel.value);
+function setYear(sel) {
+    drawBubbles(sel.value);
 }
