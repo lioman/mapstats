d53ac03103029858cedf324d93fc3e1602c02243
Lioman <dev@lioman.de>
2015-07-15 09:02:14 +0200

    merge from master

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function drawMap(year) {
+function drawMap() {
     $('.map').empty();
 
     width = 650;
@@ -17,36 +17,38 @@ function drawMap(year) {
         .attr('width', width)
         .attr('height', height);
 
-    radius = d3.scale.sqrt()
-        .domain([0, 300000000])
-        .range([0, 30]);
 
     formatNumber = d3.format('>-,');
-    d3.json('build/eu.json', function (error, eu) {
+
+    d3.json('build/eu_data.json', function (error, eu) {
         if (error) return console.error(error);
+        console.log('hier');
+        console.log(eu);
         euJson = eu;
 
+
         svg.append('path')
-            .datum(topojson.feature(eu, eu.objects.subunits))
+            .datum(topojson.feature(eu, eu.objects.countries))
             .attr('d', path);
 
         //add classes
         svg.selectAll('.subunit')
-            .data(topojson.feature(eu, eu.objects.subunits).features)
+            .data(topojson.feature(eu, eu.objects.countries).features)
             .enter().append('path')
             .attr('class', function (d) {
-                if (d.id !=-99){
-                    return 'subunit ' + d.id;}
+                if (d.id != -99) {
+                    return 'subunit ' + d.id;
+                }
                 return 'subunit NO';
             })
             .attr('d', path);
 
         // Add Label
         svg.selectAll('.subunit-label')
-            .data(topojson.feature(eu, eu.objects.subunits).features)
+            .data(topojson.feature(eu, eu.objects.countries).features)
             .enter().append('text')
             .attr('class', function (d) {
-                if (d.id !=-99){
+                if (d.id != -99) {
                     return 'subunit-label ' + d.id;
                 }
                 return 'subunit-label NO';
@@ -59,34 +61,51 @@ function drawMap(year) {
                 return d.properties.name;
             });
 
-        drawBubbles(year);
+        drawBubbles();
     });
 
 
 }
 
-function drawBubbles(year) {
+function drawBubbles() {
+
+    var year = $('#choosedYear').val(),
+        type = $('#choosedType').val();
+    var zoomRange = [0, 300000000];
+
+    if (type === 'goods') {
+        zoomRange = [0, 4300000];
+    }
+
+    var radius = d3.scale.sqrt()
+        .domain(zoomRange)
+        .range([0, 30]);
+
     $('.bubble').empty();
-    var paxYear = 'pax' + year;
+    $('.legend').empty();
+
+    var bubbleClass = 'bubble ' + type;
+
+    var dataField = type + year;
 
     svg.append('g')
-        .attr('class', 'bubble')
+        .attr('class', bubbleClass)
         .selectAll('circle')
-        .data(topojson.feature(euJson, euJson.objects.subunits).features
+        .data(topojson.feature(euJson, euJson.objects.countries).features
             .sort(function (a, b) {
-                return b.properties[paxYear] - a.properties[paxYear];
+                return b.properties[dataField] - a.properties[dataField];
             }))
         .enter().append('circle')
         .attr('transform', function (d) {
             return 'translate(' + path.centroid(d) + ')';
         })
         .attr('r', function (d) {
-            return radius(d.properties[paxYear]);
+            return radius(d.properties[dataField]);
         })
         .append('title')
         .text(function (d) {
             return d.properties.name
-                + '\nPax in ' + year + ':  ' + formatNumber(d.properties[paxYear]);
+                + '\n' + type + ' in ' + year + ':  ' + formatNumber(d.properties[dataField]);
         }).attr('class', 'bubbletext');
 
 
@@ -94,7 +113,7 @@ function drawBubbles(year) {
         .attr('class', 'legend')
         .attr('transform', 'translate(' + (width - 50) + ',' + (height - 20) + ')')
         .selectAll('g')
-        .data([3000000, 30000000, 300000000])
+        .data([zoomRange[1] / 10, zoomRange[1] / 2, zoomRange[1]])
         .enter().append('g');
 
     legend.append('circle')
@@ -110,9 +129,4 @@ function drawBubbles(year) {
         .attr('dy', '1.3em')
         .text(d3.format('.1s'));
 
-}
-
-
-function setYear(sel) {
-    drawBubbles(sel.value);
 }
